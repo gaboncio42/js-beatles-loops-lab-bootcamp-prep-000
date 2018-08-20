9199810acaf2c631260c6c004759f556027f23d8
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-08-20 01:49:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Star"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians, instruments) {
+  const array = [];
+  for(let i = 0; i < musicians.length; i++) {
+    array.push(musicians[i]  + " plays " + instruments[i]);
+  }
+  return array;
+}
+
+function johnLennonFacts(array) {
+  const facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice",
+    "foo",
+    "bar"];
+    var i = 0;
+    while(i < facts.length) {
+      facts.push(facts[i] + "!!!");
+    }
+    return facts;
+}
